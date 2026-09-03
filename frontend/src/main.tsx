ec9c78173133cf3ecba00d3bacb05af0bbc925c6
MrDoodeth <mrduves@yandex.ru>
2026-09-03 21:02:35 +0300

    fix: apply markdown list styles last

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,8 +3,8 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { AppProviders } from "./app/providers";
 import { router } from "./app/router";
-import "./index.css";
 import "github-markdown-css/github-markdown-light.css";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
